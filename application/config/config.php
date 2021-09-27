8823b503431e4451d874c129b880c184185d8424
gzuluaga <thekurai@gmail.com>
2021-09-27 10:55:52 -0500

    con httpS

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$config['base_url'] = '';
 //$config['base_url']='http://localhost:8888/';
 
-$root1 = "http://".$_SERVER['HTTP_HOST'];
+$root1 = "https://".$_SERVER['HTTP_HOST'];
 $root1 = $root1.str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = "$root1";
 //print_r($config);
